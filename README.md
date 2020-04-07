593d642b870bf396d62602d8505caf3399cd26f3
surajgurung <microbiosuraj@gmail.com>
2020-04-06 22:56:23 -0400

    edit done on timeblock changes with color in present ,future and past

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dailyPlanner
+WHEN  the planner is open
+THEN the current day is displayed at the top of the calendar
+WHEN it is scroll down
+THEN it presented with timeblocks for Military hours(9 a to 17)//
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
--- main.js
+++ main.js
@@ -6,11 +6,10 @@ currenday.text(date);
 
 var todoList = document.querySelector("#todo-list");
 
-
- var todos = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+//array is created for time block
+var todos = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 //
 
-// init();
 renderTodos();
 function renderTodos() {
     for (var i = 0; i < todos.length; i++) {
@@ -22,28 +21,28 @@ function renderTodos() {
         //create a button;
         var button = document.createElement("button");
         button.textContent = "save";
-        button.setAttribute("id", i+9);
+        button.setAttribute("id", i + 9);
         button.setAttribute("class", "save");
 
         li.appendChild(button);
 
-        
+
         //create input field;
-         var storageItem = localStorage.getItem(todos[i]);
+        var storageItem = localStorage.getItem(todos[i]);
         var inText = document.createElement("INPUT");
         inText.setAttribute("type", "text");
-        inText.setAttribute("data-time", i+9);
-        inText.setAttribute("class","myText");
-        inText.id=(i+9);
-        inText.value = storageItem;
+        inText.setAttribute("data-time", i + 9);
+        inText.setAttribute("class", "myText");
+        inText.id = (i + 9 + "t");
+        inText.value = storageItem;//
         // var input = inputs;
         // console.log(inputs);
         console.log(storageItem);
         li.appendChild(inText);
 
 
-        
-        
+
+
 
     }
 
@@ -58,19 +57,170 @@ var saveBtn = document.querySelectorAll(".save");
 for (var i = 0; i < saveBtn.length; i++) {
     saveBtn[i].addEventListener("click", function (event) {
         event.preventDefault();
-        
+
         var elementId = this.getAttribute("id");
-        var x = '[data-time="' + elementId +'"]';
+        var x = '[data-time="' + elementId + '"]';
         var inputDiv = document.querySelector(x);
         var inputText = inputDiv.value;
         console.log(inputText);
         // console.log(elementId);
-        
-        
-        // Set local storage as: 
-        // time: todo
-         localStorage.setItem(elementId, inputText);
+
+
+//setting local storage
+        localStorage.setItem(elementId, inputText);
         // Add the inputText to localStorage, along with the time in the timeblock
     });
 }
 
+
+
+
+// for 9am
+if ((new Date().getHours()) === 9) {
+    // console.log(new Date());
+
+    document.getElement('9t').style.backgroundColor = " #ff6961";
+
+} else if ((new Date().getHours()) > 9) {
+
+    document.getElementById('9t').style.backgroundColor = "#d3d3d3";
+    // document.getElementById("9t").disabled = true;
+} else {
+
+    document.getElementById('9t').style.backgroundColor = "#77dd77";
+
+}
+
+
+if ((new Date().getHours()) === 10) {
+
+
+    document.getElement('10t').style.backgroundColor = " #ff6961";
+
+} else if ((new Date().getHours()) > 10) {
+
+    document.getElementById('10t').style.backgroundColor = "#d3d3d3";
+
+} else {
+
+    document.getElementById('10t').style.backgroundColor = "#77dd77";
+
+
+}
+
+
+
+if ((new Date().getHours()) === 11) {
+
+
+    document.getElement('11t').style.backgroundColor = " #ff6961";
+
+} else if ((new Date().getHours()) > 11) {
+
+    document.getElementById('11t').style.backgroundColor = "#d3d3d3";
+
+} else {
+
+    document.getElementById('11t').style.backgroundColor = "#77dd77";
+
+}
+
+
+
+
+if ((new Date().getHours()) === 12) {
+
+
+    document.getElement('12t').style.backgroundColor = " #ff6961";
+
+} else if ((new Date().getHours()) > 12) {
+
+    document.getElementById('12t').style.backgroundColor = "#d3d3d3";
+
+} else {
+
+    document.getElementById('12t').style.backgroundColor = "#77dd77";
+
+}
+
+
+
+
+
+
+if ((new Date().getHours()) === 13) {
+
+
+    document.getElement('13t').style.backgroundColor = " #ff6961";
+
+} else if ((new Date().getHours()) > 13) {
+
+    document.getElementById('13t').style.backgroundColor = "#d3d3d3";
+
+} else {
+
+    document.getElementById('13t').style.backgroundColor = "#77dd77";
+}
+
+
+if ((new Date().getHours()) === 14) {
+
+
+    document.getElement('14t').style.backgroundColor = " #ff6961";
+
+} else if ((new Date().getHours()) > 14) {
+
+    document.getElementById('14t').style.backgroundColor = "#d3d3d3";
+
+} else {
+
+    document.getElementById('14t').style.backgroundColor = "#77dd77";
+}
+
+
+
+if ((new Date().getHours()) === 15) {
+
+
+    document.getElement('15t').style.backgroundColor = " #ff6961";
+
+} else if ((new Date().getHours()) > 15) {
+
+    document.getElementById('15t').style.backgroundColor = "#d3d3d3";
+
+} else {
+
+    document.getElementById('15t').style.backgroundColor = "#77dd77";
+}
+
+
+
+
+if ((new Date().getHours()) === 16) {
+
+
+    document.getElement('16t').style.backgroundColor = " #ff6961";
+
+} else if ((new Date().getHours()) > 16) {
+
+    document.getElementById('16t').style.backgroundColor = "#d3d3d3";
+
+} else {
+
+    document.getElementById('16t').style.backgroundColor = "#77dd77";
+}
+
+
+if ((new Date().getHours()) === 17) {
+
+
+    document.getElement('17t').style.backgroundColor = " #ff6961";
+
+} else if ((new Date().getHours()) > 17) {
+
+    document.getElementById('17t').style.backgroundColor = "#d3d3d3";
+
+} else {
+
+    document.getElementById('17t').style.backgroundColor = "#77dd77";
+}
